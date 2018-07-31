a7e1a110b7ab588876909196d84f5cce8c882b92
Artem Shishkin <artem.shishkin@corp.mail.ru>
2018-07-31 21:53:27 +0300

    cleaner

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ bool comparator(RowTable* one, RowTable* two) {
     return *one < *two;
 }
 
-int main() {
+void doit() {
     auto tableRows = new std::vector<RowTable*>;
     tableRows->push_back(new RowTable("Mai", "12", "Artem"));
     tableRows->push_back(new RowTable("Mai", "12", "Anton"));
@@ -21,9 +21,19 @@ int main() {
     tableRows->push_back(new RowTable("Ya", "50", "Artem"));
     tableRows->push_back(new RowTable("", "", "Anton"));
     tableRows->push_back(new RowTable("", "", "Artem"));
-    RowTable::print(*tableRows);
-    std::cout << "===================\n";
+//    RowTable::print(*tableRows);
+//    std::cout << "===================\n";
     std::sort(tableRows->begin(), tableRows->end(), comparator);
-    RowTable::print(*tableRows);
+    for (auto &tableRow : *tableRows)
+        delete tableRow;
+    tableRows->clear();
+    delete tableRows;
+//    RowTable::print(*tableRows);
+}
+
+int main() {
+    for (int i = 0; i < 1000000; i++) {
+        doit();
+    }
     return 0;
 }
